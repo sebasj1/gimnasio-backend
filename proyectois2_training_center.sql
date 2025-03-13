169d1b6f2e0dedbb49c07f188e31c274bfb68ade
matias jaramillo <matisebajara@outlook.com>
2025-03-13 12:35:01 -0300

    Actualizacion bd

--- proyectois2_training_center.sql
+++ proyectois2_training_center.sql
@@ -2,8 +2,9 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Generation Time: Jul 11, 2024 at 02:31 AM
--- Server version: 10.6.17-MariaDB
+-- Host: mysql-proyectois2.alwaysdata.net
+-- Generation Time: Mar 09, 2025 at 04:55 PM
+-- Server version: 10.6.18-MariaDB
 -- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -17,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `training_center`
+-- Database: `proyectois2_training_center`
 --
 
 -- --------------------------------------------------------
@@ -30,27 +31,26 @@ CREATE TABLE `mensaje` (
   `id_mensaje` int(11) NOT NULL,
   `nombre` varchar(70) NOT NULL,
   `apellido` varchar(70) NOT NULL,
-  `numero_celular` varchar(20) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
   `whatsapp` varchar(4) NOT NULL DEFAULT 'si',
   `contenido` varchar(300) NOT NULL DEFAULT '',
-  `fecha_hora` datetime NOT NULL DEFAULT current_timestamp(),
+  `fecha_hora` timestamp NOT NULL DEFAULT current_timestamp(),
   `entrenaba` varchar(50) NOT NULL DEFAULT 'No aun',
   `email` varchar(50) DEFAULT NULL,
- `edad` int(11) NOT NULL
-
+  `edad` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Dumping data for table `mensaje`
 --
 
-INSERT INTO `mensaje` (`id_mensaje`, `nombre`, `apellido`, `numero_celular`, `whatsapp`, `contenido`, `fecha_hora`, `entrenaba`, `email`,`edad`) VALUES
-(1, 'Pablo', 'Elias', '2352342', 'no', 'hola...', '2024-07-10 06:45:08', 'No aun', NULL,27),
-(2, 'Mario', 'Bros', '43543334', 'si', 'hola quiero consultar...', '2024-07-10 06:45:08', 'No aun', NULL,28),
-(4, 'Pedro', 'Sosa', NULL, 'no', 'Hola, cuanto es el plan mensual...', '2024-07-10 03:52:47', 'Aun entreno', NULL,30),
-(6, 'Marina', 'Sotelo', '43543332', 'si', 'hola quiero consultar...', '2024-07-10 03:54:50', 'No aun', NULL,33),
-(8, 'Julio', 'Lopez', '4354333', 'no', 'hola quiero consultar...', '2024-07-10 05:49:46', 'Aun si', 'julio@gmail.com',22),
-(9, 'Pepe', 'Argento', '354232346', 'si', 'hola...', '2024-07-10 06:45:08', 'No aun', NULL,25);
+INSERT INTO `mensaje` (`id_mensaje`, `nombre`, `apellido`, `telefono`, `whatsapp`, `contenido`, `fecha_hora`, `entrenaba`, `email`, `edad`) VALUES
+(1, 'Pablo', 'Elias', '2352342', 'no', 'hola...', '2024-07-09 22:00:00', 'No aun', NULL, 20),
+(2, 'Mario', 'Bros', '43543334', 'si', 'hola quiero consultar...', '2024-07-09 22:00:00', 'No aun', NULL, 28),
+(4, 'Pedro', 'Sosa', NULL, 'no', 'Hola, cuanto es el plan mensual...', '2024-07-09 22:00:00', 'Aun entreno', NULL, 30),
+(6, 'Marina', 'Sotelo', '43543332', 'si', 'hola quiero consultar...', '2024-07-09 22:00:00', 'No aun', NULL, 23),
+(8, 'Julio', 'Lopez', '4354333', 'no', 'hola quiero consultar...', '2024-07-09 22:00:00', 'Aun si', 'julio@gmail.com', 24),
+(9, 'Pepe', 'Argento', '354232346', 'si', 'hola...', '2024-07-09 22:00:00', 'No aun', NULL, 26);
 
 -- --------------------------------------------------------
 
@@ -69,8 +69,8 @@ CREATE TABLE `plan` (
 
 INSERT INTO `plan` (`id_tipo_plan`, `descripcion`) VALUES
 (1, 'Basico'),
-(2, 'Semanal'),
-(3, 'Mensual'),
+(2, 'PRO'),
+(3, 'Elite'),
 (4, 'Ninguno');
 
 -- --------------------------------------------------------
@@ -84,16 +84,6 @@ CREATE TABLE `rutina` (
   `descripcion` varchar(1500) NOT NULL,
   `id_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
---
--- Dumping data for table `rutina`
---
-
-INSERT INTO `rutina` (`id_rutina`, `descripcion`, `id_usuario`) VALUES
-(1, 'Para empezar .....', 1),
-(2, 'Para aumentar masa muscular ....', 2),
-(9, 'Brazos y piernas ....', 3),
-(10, 'Musculos....', 18);
 
 -- --------------------------------------------------------
 
@@ -118,6 +108,19 @@ INSERT INTO `tipo_usuario` (`id_tipo_usuario`, `descripcion`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `turno`
+--
+
+CREATE TABLE `turno` (
+  `id_turno` int(11) NOT NULL,
+  `dia` varchar(10) NOT NULL,
+  `hora` varchar(6) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `usuario`
 --
 
@@ -130,21 +133,10 @@ CREATE TABLE `usuario` (
   `fecha_nacimiento` date NOT NULL,
   `telefono` varchar(20) DEFAULT NULL,
   `email` varchar(40) DEFAULT NULL,
+  `password` varchar(70) NOT NULL,
   `id_tipo_plan` int(11) DEFAULT NULL,
   `id_tipo_usuario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
---
--- Dumping data for table `usuario`
---
-
-INSERT INTO `usuario` (`id_usuario`, `nombre`, `apellido`, `numero_documento`, `direccion`, `fecha_nacimiento`, `telefono`, `email`, `id_tipo_plan`, `id_tipo_usuario`) VALUES
-(1, 'Juan', 'Perez', '4653333', 'Las margaritas 444', '2000-07-02', NULL, NULL, 1, 2),
-(2, 'Noelia', 'Barreto', '4345454', 'Don Bosco 445', '2001-07-02', NULL, NULL, 1, 1),
-(3, 'Roman', 'Riquelme', '4456664', 'Catamarca 999', '1998-07-02', NULL, NULL, 1, 1),
-(6, 'Luis', 'Lopez', '32452342', 'Las margaritas 554', '2000-07-02', NULL, NULL, 4, 2),
-(17, 'Maria', 'Ramirez', '3245234s2', 'Las margaritas 554', '2000-07-02', NULL, NULL, 4, 3),
-(18, 'Lorenzo', 'Bordon', '3464575', 'Murcia 987', '1990-10-10', NULL, NULL, 1, 1);
 
 --
 -- Indexes for dumped tables
@@ -176,6 +168,13 @@ ALTER TABLE `tipo_usuario`
   ADD PRIMARY KEY (`id_tipo_usuario`);
 
 --
+-- Indexes for table `turno`
+--
+ALTER TABLE `turno`
+  ADD PRIMARY KEY (`id_turno`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
 -- Indexes for table `usuario`
 --
 ALTER TABLE `usuario`
@@ -192,7 +191,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT for table `mensaje`
 --
 ALTER TABLE `mensaje`
-  MODIFY `id_mensaje` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_mensaje` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `plan`
@@ -213,10 +212,16 @@ ALTER TABLE `tipo_usuario`
   MODIFY `id_tipo_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT for table `turno`
+--
+ALTER TABLE `turno`
+  MODIFY `id_turno` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Constraints for dumped tables
@@ -227,6 +232,12 @@ ALTER TABLE `usuario`
 --
 ALTER TABLE `rutina`
   ADD CONSTRAINT `rutina_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `turno`
+--
+ALTER TABLE `turno`
+  ADD CONSTRAINT `turno_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `usuario`
